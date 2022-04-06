fd04a9b970bf6fca8208b56560a2618292c12d88
Stidz-PC <marszp8888@gmail.com>
2022-04-06 21:57:22 +0200

    Working Rest API with hibernate

---
+++ src/main/java/com/example/springboothibernatedemo/service/EmployeeService.java
@@ -0,0 +1,2 @@
+package com.example.springboothibernatedemo.service;public interface EmployeeService {
+}
---
+++ src/main/java/com/example/springboothibernatedemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,2 @@
+package com.example.springboothibernatedemo.service;public class EmployeeServiceImpl {
+}
